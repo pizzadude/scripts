b726420aa95761992ca7fddedf764208d673257e
PizzaDude <pizzadudedotca@gmail.com>
2021-10-19 20:08:38 +0000

    Add files via upload

--- simplesysinfoscript.py
+++ simplesysinfoscript.py
@@ -3,7 +3,7 @@ import os
 import subprocess
 with open("/proc/uptime", encoding="utf-8") as uptime_file:
         uptime, _ = uptime_file.read().strip().split()
-        print(f"Uptime: {float(uptime) // 3600} hour(s)")
+        print(f"Uptime:  {float(uptime) // 3600} hour(s)")
 encoding = 'utf-8'
 seLinucks = subprocess.run(
     args = ["getenforce"],
